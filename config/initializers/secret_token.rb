1012ff559e8391149dda5e608df03da8ad94b9c8
Syunsuke Mihara <mihara.git@gmail.com>
2013-09-22 22:09:07 +0900

    rails project start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SearchLight::Application.config.secret_token = '53e078a7afc353010782de2d370e21aea6203161b55fca6868e13f4f22b1905e9253d830e73f8d9b9fc8f51785a6a5acd4098079370624371e12d524ff2f2dfc'
